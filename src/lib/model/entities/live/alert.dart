a3fd56091167ca2e1dade9dcfe1cd90c5d1d411b
André Júlio Moreira <andre.moreira.9@hotmail.com>
2022-04-19 16:02:14 +0100

    Downgrade flutter version and merge with uni

---
+++ src/lib/model/entities/live/alert.dart
@@ -0,0 +1,13 @@
+import 'package:uni/model/entities/live/alert_type.dart';
+import 'package:uni/model/general_alert.dart';
+
+class Alert extends GeneralAlert {
+  final AlertType _alertType;
+
+  Alert(DateTime startTime, DateTime finishTime, this._alertType)
+      : super(startTime, finishTime);
+
+  AlertType getGeneralAlert() {
+    return _alertType;
+  }
+}
