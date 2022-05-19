9fd8b9199753ebd30f7ef0ab898a164a4d3af57d
Nuno Alves <nyansword@gmail.com>
2022-05-19 12:03:48 +0100

    fix: unused fields

--- src/lib/view/Widgets/validation_buttons.dart
+++ src/lib/view/Widgets/validation_buttons.dart
@@ -3,16 +3,16 @@ import 'package:uni/controller/alert/alert_controller_interface.dart';
 
 class ValidationButtons extends StatefulWidget {
   final MainAxisAlignment _mainAxisAlignment;
-  final AlertControllerInterface _alertController;
-  final String _spontaneousAlertId;
+  //final AlertControllerInterface _alertController;
+  //final String _spontaneousAlertId;
   const ValidationButtons(
       {Key key,
       MainAxisAlignment mainAxisAlignment = MainAxisAlignment.center,
       AlertControllerInterface alertController,
       String spontaneousAlertId})
       : _mainAxisAlignment = mainAxisAlignment,
-        _alertController = alertController,
-        _spontaneousAlertId = spontaneousAlertId,
+        //_alertController = alertController,
+        //_spontaneousAlertId = spontaneousAlertId,
         super(key: key);
 
   @override
