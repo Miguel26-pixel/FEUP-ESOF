c4dde05ed420374a96f307af9f5a31c465526bb2
André Júlio Moreira <andre.moreira.9@hotmail.com>
2022-05-21 12:42:47 +0100

    Fix alerts get request filtering outdated alerts

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -4,6 +4,13 @@ const express = require('express');
 const {seed} = require("./mock");
 const { firestore } = require("firebase-admin");
 
+const RADIUS_METERS = 1000;
+const DEFAULT_POSITION = {
+    latitude: 41.177787,
+    longitude: -8.595922,
+}
+
+
 const app = express();
 app.use(express.json());
 
@@ -48,16 +55,36 @@ exports.pointsOfInterest = functions.https.onRequest(async (_, response) => {
 });
 
 app.get("/point/:id/alerts", async (req, res) => {
-    const point = (await pointsCollection.doc(req.params.id).get()).data();
-    const alerts = await Promise.all(point.alerts.map(async (alertRef) => {
-        const alert = (await alertRef.get()).data();
-        console.log(alert)
+    const pointRef = pointsCollection.doc(req.params.id);
+    const pointSnapshot = await pointRef.get();
+    if (!pointSnapshot.exists) {
+        return res.status(401).json({error: "Not found"});
+    }
+
+    const point = pointSnapshot.data();
+    const alerts = await point.alerts.reduce(async (result, alertRef) => {
+        const alertSnapshot = await alertRef.get();
+        const alert = alertSnapshot.data();
+
+        if (alert["start-time"].toDate() > new Date()) {
+            return result;
+        }
+
+        if (alert["finish-time"].toDate() < new Date()) {
+            await pointRef.update({
+                alerts: firestore.FieldValue.arrayRemove(alertRef),
+            })
+            return result;
+        }
+
         const type = (await alert.type.get()).data();
-        return {
+        result.push({
             ...alert,
             type,
-        };
-    }));
+        });
+
+        return result;
+    }, []);
 
     return res.json({
         data: alerts
@@ -94,6 +121,72 @@ app.post("/points/:id/alerts/new", async (req, res) => {
 
     return res.json("");
 });
+
+app.get("/alerts/spontaneous", async (req, res) => {
+    const locationRaw = req.body.location || DEFAULT_POSITION;
+    const floor = req.body.floor || 0;
+
+    const bounds = geofire.geohashQueryBounds([
+        locationRaw.latitude,
+        locationRaw.longitude,
+    ], RADIUS_METERS);
+
+    const pointsQueries = [];
+    const groupsQueries = [];
+
+    for (const bound of bounds) {
+        const pointQuery = pointsCollection
+            .where("geohash", "!=", null)
+            .orderBy("geohash")
+            .where("floor", "=", floor)
+            .startAt(bound[0])
+            .endAt(bound[1])
+            .get();
+
+        pointsQueries.push(pointQuery);
+        groupsQueries.push(groupQuery);
+    }
+    
+
+    let pointsData = (await Promise.all(pointsQueries)).map(q => q.docs).flat();
+    let groupsData = (await Promise.all(groupsQueries)).map(q => q.docs).flat();
+
+    const groups = await Promise.all(groupsData.map(async (obj) => {
+        const data = obj.data();
+
+        const points = await Promise.all(data.points.map(async (pointRef) => {
+            const data = (await pointRef.get()).data();
+            const alerts = data.alerts.map((alertRef) => alertRef.id);
+    
+            return {
+                ...data,
+                alerts,
+                id: obj.id
+            }
+        }));
+
+        return {
+            ...data,
+            points,
+        }
+    }))
+
+    const points = pointsData.map((obj) => {
+        const data = obj.data();
+        const alerts = data.alerts.map((alertRef) => alertRef.id);
+    
+        return {
+            ...data,
+            alerts,
+            id: obj.id
+        }
+    });
+  
+    res.json({
+        points,
+        groups,
+    })
+})
 
 exports.widgets = functions.https.onRequest(app);
 
