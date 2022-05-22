aec383d90bdf7379c5b20e32c39fbad7631b6db4
MartaMariz <up201907020@fe.up.pt>
2022-05-22 23:16:28 +0100

    working but bugs

--- src/lib/controller/poi/type_poi_controller_interface.dart
+++
@@ -1,9 +0,0 @@
-import 'package:uni/model/entities/live/alert_type.dart';
-import 'package:uni/model/entities/live/poi_type.dart';
-import 'package:uni/model/entities/live/point.dart';
-import 'package:latlong2/latlong.dart';
-
-
-abstract class TypeOfPOIControllerInterface {
-  Future<String> getName();
-}
--- src/lib/view/Pages/create_poi_view.dart
+++ src/lib/view/Pages/create_poi_view.dart
@@ -13,8 +13,6 @@ class CreatePOIPage extends StatefulWidget {
 
   CreatePOIPage(){}
 
-
-
   @override
   State<StatefulWidget> createState(){
     return _CreatePOIPageState();
@@ -22,16 +20,18 @@ class CreatePOIPage extends StatefulWidget {
 }
 
 
-
 class _CreatePOIPageState extends GeneralPageViewState {
   
   _CreatePOIPageState(){
+  }
+  @override
+  void initState() {
     getFloorLimit();
     print("herea aa");
     getPOITypes();
     print(poiTypes.length);
-
-  }
+   {
+  }}
 
   final _formKey = GlobalKey<FormState>();
 
@@ -42,8 +42,9 @@ class _CreatePOIPageState extends GeneralPageViewState {
 
   List<bool> isSelected = [];
   List<PointOfInterestType> poiTypes = [];
-  List<int> floorLimits = [0,9];
+  List<int> floorLimits = [];
   int currvalue = 0;
+  int pressed = 0;
 
   InputDecoration textFieldDecoration(String placeholder) {
     return InputDecoration(
@@ -75,6 +76,7 @@ class _CreatePOIPageState extends GeneralPageViewState {
       poiTypes = [... response];
       print("SIZE OF POITUPE");
       print(poiTypes.length);
+      pressed = poiTypes.length -1;
       if (poiTypes.first.getIcon() != null){
         print("YES ICON");
       }
@@ -99,41 +101,63 @@ class _CreatePOIPageState extends GeneralPageViewState {
     );
   }
 
-  Widget getPOIName() => TextField(
+  Widget getPOIName() => TextFormField(
     decoration: InputDecoration(
       labelText: 'Name',
       hintText: 'Name',
       contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+
     ),
+    validator: (value) {
+      if (value == null || value.isEmpty) {
+        return 'Please enter some text';
+      }
+      return null;
+    },
+
       controller: nameController,
   );
 
-  Widget getPOILatitude() => TextField( 
+  Widget getPOILatitude() => TextFormField(
       decoration: InputDecoration(
         labelText: 'Latitude',
         hintText: 'Latitude',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
       ),
+    validator: (value) {
+      if (value == null || value.isEmpty || !(value.runtimeType == double || value.runtimeType == int) ||
+          value as double <=  -90 || value as double >=90) {
+        return 'Invalid input, please enter a number between -90 and 90';
+      }
+      return null;
+    },
       controller: latitudeController,
   );
   
-  Widget getPOILongitude() => TextField( 
+  Widget getPOILongitude() => TextFormField(
       decoration: InputDecoration(
         labelText: 'Longitude',
         hintText: 'Longitude',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
       ),
       controller: longitudeController,
+      validator: (value) {
+      if (value == null || value.isEmpty || !(value.runtimeType == double || value.runtimeType == int) ||
+          value as double <=  -90 || value as double >=90) {
+        return 'Invalid input, please enter a number between -90 and 90';
+      }
+      return null;
+    },
   );
   
-  Widget getPOIFloor() =>
-      Align(
+  Widget getPOIFloor(){
+    return Align(
         alignment: Alignment.center,
         child: Column(
           children: <Widget>[getTitle("Select Floor:", Icon(
-          Icons.elevator_rounded ,
-          color: Theme.of(context).accentColor,
-        )),
+            Icons.elevator_rounded ,
+            color: Theme.of(context).accentColor,
+          )),
             NumberPicker(
               value: currvalue,
               minValue: floorLimits.first,
@@ -143,7 +167,16 @@ class _CreatePOIPageState extends GeneralPageViewState {
             ),
           ],
         )
-      );
+    );
+
+  }
+
+    setWidgetColor(int i){
+    if (isSelected[i]){
+      return Theme.of(context).accentColor;
+    }
+    return Colors.black;
+  }
 
   List<Widget> getTypesPoiWidgets(){
     final List<Widget> widgets= [];
@@ -152,54 +185,99 @@ class _CreatePOIPageState extends GeneralPageViewState {
 
     print(poiTypes.length);
 
-    poiTypes.forEach((type) {
-      print('add widget');
+    for (int i = 0 ; i<poiTypes.length ; i++){
       widgets.add(
           Column(
             children: [
               Icon(
-                  type.getIcon()
+                poiTypes[i].getIcon(),
+                //color: setWidgetColor(i),
+                size: 40,
               ),
-              Text(type.getName())
+              Expanded(child: Text(
+                poiTypes[i].getName(),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis, // and this
+              )),
             ],
           ));
+    }
 
-      print("here");
-      /*print("there is a poi");
-      if ( type.getIcon() != null){
-        print("there is icon ");
-      }
-      if (num < 3) {
-        row.add(Icon(
-            type.getIcon()
-        ));
-      }
-      else{
-        widgets.add(Row(
-          children: row,
-        ));
 
-        row = [];
-        row.add(Icon(
-            type.getIcon()
-        ));
-        num = 1;
-      }
-      num++;*/
-    });
-
-   /* if (widgets.isEmpty){
-      throw Exception("NO types of points of interest");
-    }*/
     print("sizes");
     print(isSelected.length);
     print(poiTypes.length);
     return widgets;
   }
 
-  Widget getTypesPOI(){
+  void onControlPress(int index){
+    isSelected[index] = true;
+    print(index);
+    pressed = index;
+    for (int buttonIndex = 0; buttonIndex <
+        isSelected.length; buttonIndex++) {
+      if (buttonIndex == index) {
+        print("SLECTED ");
+        print(buttonIndex);
 
-    /*return GridView.count(
+        isSelected[buttonIndex] = true;
+      } else {
+        isSelected[buttonIndex] = false;
+      }
+    }
+    int s = 0;
+    for( bool a in isSelected){
+      print(s);
+      s++;
+      if (a){
+        print('true');
+      }
+    }
+  }
+
+  Widget getTypesPOI(){
+    var counter = 0;
+    getPOITypes();
+
+    return GridView.count(
+        primary: false,
+        padding: EdgeInsets.fromLTRB(40, 30, 40, 0),
+        shrinkWrap: true,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+        crossAxisCount: 3,
+        children: getTypesPoiWidgets().map((widget) {
+          final index = ++counter - 1;
+
+          print("selected");
+          print(index);
+          print(isSelected[index]);
+
+          return ToggleButtons(
+            onPressed: (_) => onControlPress(index),
+            fillColor: Colors.white60,
+            renderBorder: false,
+            children: [widget],
+            isSelected: [isSelected[index]],
+            selectedColor: Theme.of(context).accentColor,
+
+
+          );
+        }).toList());
+/*
+    List<bool> selected = [false, false, false, false];
+
+    return GridView.count(
+        crossAxisCount: 2,
+        children: [
+          Icon(Icons.info),
+          Icon(Icons.title),
+          Icon(Icons.info),
+          Icon(Icons.title)
+        ]);
+
+
+    return GridView.count(
         crossAxisCount: 2,
         children: getTypesPoiWidgets().map((widget) {
           final index = ++counter - 1;
@@ -224,7 +302,7 @@ class _CreatePOIPageState extends GeneralPageViewState {
                 },
             children: [widget],
           );
-        }).toList());*/
+        }).toList());
       return ToggleButtons(
       children: getTypesPoiWidgets(),
       onPressed: (int index) {
@@ -246,7 +324,7 @@ class _CreatePOIPageState extends GeneralPageViewState {
       fillColor: Colors.white60,
       renderBorder: false,
       isSelected: isSelected,
-    );
+    );*/
   }
 
 
@@ -254,91 +332,106 @@ class _CreatePOIPageState extends GeneralPageViewState {
     return Form(
       key: _formKey,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          getTitle("Select name: ", Icon(
-            Icons.add_business_rounded,
-            color: Theme.of(context).accentColor,
-          )),
-          getPOIName(),
-          getTitle("Select latitude:",Icon(
-            Icons.add_location,
-            color: Theme.of(context).accentColor,
-          )),
-          getPOILatitude(),
-          getTitle("Select longitude:",Icon(
-            Icons.add_location,
-            color: Theme.of(context).accentColor,
-          )),
-          getPOILongitude(),
-          getPOIFloor(),
-          getTitle("Select type:", Icon(
-            Icons.add_location,
-            color: Theme.of(context).accentColor,
-          )),
-          getTypesPOI(),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            getTitle("Select name: ", Icon(
+          Icons.add_business_rounded,
+          color: Theme.of(context).accentColor,
+        )),
+        getPOIName(),
+        getTitle("Select latitude:",Icon(
+          Icons.add_location,
+          color: Theme.of(context).accentColor,
+        )),
+        getPOILatitude(),
+        getTitle("Select longitude:",Icon(
+          Icons.add_location,
+          color: Theme.of(context).accentColor,
+        )),
+        getPOILongitude(),
+        getPOIFloor(),
+        getTitle("Select type:", Icon(
+          Icons.add_location,
+          color: Theme.of(context).accentColor,
+        )),
+    ]
+    )
+    );
+  }
 
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0,
-                horizontal:  15),
-            child: ElevatedButton(
-              onPressed: () {
-                print("here");
+  bool submit(){
+    if (_formKey.currentState.validate()) {
 
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
+    int index = isSelected.indexOf(true);
+    PointOfInterestType poi = poiTypes[index];
+    print('type');
+    print(index);
+    print('name');
+    print(nameController.text);
+    print('floor');
+    final double latitude = double.parse(latitudeController.text);
+    final double longitude = double.parse(longitudeController.text);
+    final int floor = currvalue;
+    print(floor);
+    bool submited = false;
 
-                int index = isSelected.indexOf(true);
-                PointOfInterestType poi = poiTypes[index];
-                print('type');
-                print(index);
-                print('name');
-                print(nameController.text);
-                print('floor');
-                final double latitude = double.parse(latitudeController.text);
-                final double longitude = double.parse(longitudeController.text);
-                final int floor = currvalue;
-                print(floor);
+    MockPointOfInterestController().createPOI(
+        nameController.text, LatLng(latitude, longitude),floor, poi)
+        .then((response) {
+      if (response){
+        print('added poi');
+        submited = true;
+      }
+    });
 
-                final createPOI = MockPointOfInterestController().createPOI(
-                    nameController.text, LatLng(latitude, longitude),floor, poi);
+    return submited;
+  }
 
-                createPOI.then((response) {
-                  if (response){
-                    print('added poi');
-                  }
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pop();
+  Widget getButton(){
 
-                  Navigator.pushNamed(context, '/' + Constants.navLive);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20,
+          horizontal:  100),
+      child: ElevatedButton(
+        onPressed: () {
+            if (submit()){
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
 
+              Navigator.pushNamed(context, '/' + Constants.navLive);
+            }
+            else{
+              print('ERROR');
+            }
 
-                });
-              },
-              child: const Text('Create Point of interest'),
-            ),
-          ),
-        ],
+          },
+        child: const Text('Create Point of interest'),
       ),
     );
   }
+
   
   @override
   Widget getBody(BuildContext context) {
 /*
     return Container(
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Text("Hello")
-      )
-    );
-  }*/
+        child: PageView(
+          children: [getMockGrid()],
+        )
+    );*/
+
 
     return  Container(
       child:  Align(
         alignment: AlignmentDirectional.center,
           child: ListView(
             children: [ PageTitle(name: Constants.navCreatePoi),
-              getForm()],
+              getForm(), getTypesPOI(), getButton()],
           )
       )
     );
