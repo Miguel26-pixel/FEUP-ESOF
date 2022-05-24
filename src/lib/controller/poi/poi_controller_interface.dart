9db15234df6718b9e95f7c1ffd0fde690fa507bf
MartaMariz <up201907020@fe.up.pt>
2022-05-24 16:27:53 +0100

    fixed unused import

--- src/lib/controller/poi/poi_controller_interface.dart
+++ src/lib/controller/poi/poi_controller_interface.dart
@@ -1,4 +1,3 @@
-import 'package:uni/model/entities/live/alert_type.dart';
 import 'package:uni/model/entities/live/point.dart';
 import 'package:uni/model/entities/live/poi_type.dart';
 import 'package:latlong2/latlong.dart';
